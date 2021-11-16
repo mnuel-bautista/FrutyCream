7208ba09d940dfa8edcf971721e20bb16dc5243b
Manuel Bautista <garcia.manuel.b7@gmail.com>
2021-11-16 14:53:48 -0600

    agregar barra de navegación lateral

---
+++ src/admin/menu/menu.php
@@ -0,0 +1,24 @@
+<div class="header">
+            <nav class="nav-bar">
+                <li><a href="#" class="mdc-icon-button material-icons-outlined nav-selected-item">
+                    <span>home</span>
+                    <p class="s1">Inicio</p>
+                </a></li>
+                <li><a href="#" class="mdc-icon-button material-icons-outlined">
+                    <span>shopping_bag</span>
+                    <p class="s1">Ventas</p>
+                </a></li>
+                <li><a href="#" class="mdc-icon-button material-icons-outlined">
+                    <span>inventory_2</span>
+                    <p class="s1">Productos</p>
+                </a></li>
+                <li><a href="#" class="mdc-icon-button material-icons-outlined">
+                    <span>money_off</span>
+                    <p class="s1">Promociones</p>
+                </a></li>
+                <li><a href="http://localhost/proyecto-pw/src/admin/users/users.php" class="mdc-icon-button material-icons-outlined">
+                    <span>person</span>
+                    <p class="s1">Usuarios</p>
+                </a></li>
+            </nav>
+</div>
