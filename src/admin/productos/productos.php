8e37ce9ee8a6f38d9d48254de2bfff03a12faa33
Manuel Bautista <garcia.manuel.b7@gmail.com>
2021-11-16 15:52:02 -0600

    Añadir pagina de producto

---
+++ src/admin/productos/productos.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
+    <link href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css" rel="stylesheet">
+    <script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="../css/productos.css">
+    <title>Productos</title>
+</head>
+<body>
+    <div class="sidebar">
+    <div class="header">
+            <nav class="nav-bar">
+                <li><a href="#" class="mdc-icon-button material-icons-outlined">
+                    <span>home</span>
+                    <p class="s1">Inicio</p>
+                </a></li>
+                <li><a href="#" class="mdc-icon-button material-icons-outlined">
+                    <span>shopping_bag</span>
+                    <p class="s1">Ventas</p>
+                </a></li>
+                <li><a href="#" class="mdc-icon-button material-icons-outlined elemento-seleccionado">
+                    <span>inventory_2</span>
+                    <p class="s1">Productos</p>
+                </a></li>
+                <li><a href="#" class="mdc-icon-button material-icons-outlined">
+                    <span>money_off</span>
+                    <p class="s1">Promociones</p>
+                </a></li>
+                <li><a href="http://localhost/proyecto-pw/src/admin/users/users.php" class="mdc-icon-button material-icons-outlined">
+                    <span>person</span>
+                    <p class="s1">Usuarios</p>
+                </a></li>
+            </nav>
+</div>
+    </div>
+</body>
+</html>
