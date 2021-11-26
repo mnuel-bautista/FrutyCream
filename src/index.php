7a25282c73083a2fc7ea574ddb89e2df628dc0bd
Manuel Bautista <garcia.manuel.b7@gmail.com>
2021-11-25 22:37:59 -0600

    Agregar icono

--- src/index.php
+++ src/index.php
@@ -16,6 +16,9 @@ $productos = mysqli_query($conn, $consulta)->fetch_all(MYSQLI_ASSOC);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/menu-inicio.css">
     <link rel="stylesheet" href="css/styles.css">
     <link rel="shortcut icon" href="img/icon1.ico" />
@@ -25,7 +28,9 @@ $productos = mysqli_query($conn, $consulta)->fetch_all(MYSQLI_ASSOC);
 <body>
     <div class="hero">
         <nav>
-            <div class="logo"></div>
+            <div class="logo">
+                <p>La michoacana</p>
+            </div>
             <ul>
                 <li><a href="index.html" class="active">Inicio</a></li>
                 <li><a href="Nosotros.html">Nosotros</a></li>
