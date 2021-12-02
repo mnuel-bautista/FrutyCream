09f5d437da24a34bff94f74172bc22a9610ba230
Manuel Bautista <garcia.manuel.b7@gmail.com>
2021-12-01 23:38:35 -0600

    Arreglar el error de la pagina de inicio

--- src/admin/productos.php
+++ src/admin/productos.php
@@ -1,36 +1,44 @@
-
 <?php
-ini_set('display_errors', '1'); 
-ini_set('display_startup_errors', '1'); 
-error_reporting(E_ALL); 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
 
-$conn = mysqli_connect('localhost', 'root', '', 'paleteria'); 
+$conn = mysqli_connect('localhost', 'root', '', 'paleteria');
 
-$consulta = 'SELECT * FROM producto'; 
+$consulta = 'SELECT p.id_producto, p.nombre, p.precio, p.img, c.categoria FROM producto p INNER JOIN categoria c ON p.id_cat = c.id_cat';
 
-if(isset($_GET['categoryId'])) {
+if (isset($_GET['categoryId']) && !empty($_GET['categoryId'] )) {
     //If the url contains a parameter specifying the category, then filter the result. 
-    $category_id = $_GET['categoryId']; 
-    $consulta = $consulta." where id_cat = ".$category_id.";"; 
+    
+    $category_id = $_GET['categoryId'];
+    $consulta = $consulta . " where p.id_cat = " . $category_id . ";";
 } else {
-    $consulta = $consulta.";"; 
+    $consulta = $consulta . ";";
 }
 
-$result = mysqli_query($conn, $consulta); 
+$result = mysqli_query($conn, $consulta);
 
-$products = mysqli_fetch_all($result); 
+
+$products = $result->fetch_all(MYSQLI_ASSOC);
 
 
 ?>
 
-<?php foreach($products as $product): ?>
-    <li class="producto" id="<?=$product[0]?>">
-        <button class="mdc-icon-button material-icons-outlined añadir-articulo"><span class="material-icons-outlined">add_circle_outline</span><div class="mdc-icon-button__ripple"></div></button>
-        <img src="img/helado_fresa.png" alt="">
-            <div>
-                <p class="s1"><?= $product[1] ?></p>
-                <p class="s1"><?= $product[3] ?></p>
-            </div>
+<?php foreach ($products as $product) : ?>
+    <li class="producto" id="<?= $product['id_producto'] ?>">
+
+        <div class="imagen">
+            <div class="capa"></div>
+            <img class="img" src="<?= "../" . $product['img'] ?>" alt="">
+        </div>
+        <div>
+            <p class="categoria"><?= $product['categoria'] ?></p>
+            <p class="nombre s1"><?= $product['nombre'] ?></p>
+            <p class="precio s1"><?= $product['precio'] ?></p>
+        </div>
+        <button class="mdc-icon-button material-icons-outlined añadir-articulo"><span class="material-icons-outlined">add_circle_outline</span>
+            <div class="mdc-icon-button__ripple"></div>
+        </button>
     </li>
 <?php endforeach; ?>
